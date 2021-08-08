b94d4f1a78107ebc911613903ffb16f19d0c731f
Hyeyeon Park <hykelly7721@gmail.com>
2021-08-08 21:53:57 +0900

    #refactor fullpage.js 수정

--- animation 02/js/main.js
+++ animation 02/js/main.js
@@ -245,32 +245,32 @@ function checkOnlyOne(chk){
 }
 
 // fullpage scroll event
-window.addEventListener("wheel", function(e){
-	e.preventDefault();
-},{passive : false});
+// window.addEventListener("wheel", function(e){
+// 	e.preventDefault();
+// },{passive : false});
 
-var $html = $("html");
-var page = 1;
-var lastPage = $(".wrap_event").length;
+// var $html = $("html");
+// var page = 1;
+// var lastPage = $(".wrap_event").length;
 
-$html.animate({scrollTop:0},10);
+// $html.animate({scrollTop:0},10);
 
-$(window).on("wheel", function(e){
-	if( $html.is(":animated") ) return;
+// $(window).on("wheel", function(e){
+// 	if( $html.is(":animated") ) return;
 
-  if( e.originalEvent.deltaY > 0) {
+//   if( e.originalEvent.deltaY > 0) {
 
-    if(page == lastPage) return;
-		page++;
-  }
+//     if(page == lastPage) return;
+// 		page++;
+//   }
   
-    else if( e.originalEvent.deltaY < 0 ) {
+//     else if( e.originalEvent.deltaY < 0 ) {
 
-    if(page == 1) return;
-		page--;
-	}
+//     if(page == 1) return;
+// 		page--;
+// 	}
 
-	var posTop = (page-1) * $(window).height();
+// 	var posTop = (page-1) * $(window).height();
 
-  $html.animate({scrollTop : posTop});
-});
+//   $html.animate({scrollTop : posTop});
+// });
